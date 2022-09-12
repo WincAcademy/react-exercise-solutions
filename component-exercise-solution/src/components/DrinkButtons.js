cd32cbe70467ca841035ea165d3285d74e0376ed
Mark van der Laan <mrkvdl@gmail.com>
2022-09-12 12:29:29 +0200

    add component exercise solution

---
+++ component-exercise-solution/src/components/DrinkButtons.js
@@ -0,0 +1,13 @@
+import './DrinkButtons.css';
+
+export const DrinkButtons = () => {
+	return (
+		<>
+			<h2>Would you like tea or coffee?</h2>
+			<div className="Button-group">
+				<button className="Button">Tea</button>
+				<button className="Button">Coffee</button>
+			</div>
+		</>
+	);
+};
