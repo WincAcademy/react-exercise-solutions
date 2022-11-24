77eec402819138e6ebb45454a745243a0516014b
Mark van der Laan <mrkvdl@gmail.com>
2022-11-24 13:18:09 +0100

    fix: listloop exercise ID's

--- listloop-exercise-solution/src/utils/data.js
+++ listloop-exercise-solution/src/utils/data.js
@@ -1,40 +1,40 @@
 const tea = {
-	id: 1,
+	id: self.crypto.randomUUID(),
 	name: 'Tea',
 	imgUrl: 'https://media.wincacademy.nl/tea.jpeg',
 	alt: `Picture of a cup of tea`,
 };
 
 const coffee = {
-	id: 2,
+	id: self.crypto.randomUUID(),
 	name: 'Coffee',
 	imgUrl: 'https://media.wincacademy.nl/coffee.jpeg',
 	alt: `Picture of a cup coffee`,
 };
 
 const kombucha = {
-	id: 3,
+	id: self.crypto.randomUUID(),
 	name: 'Kombucha',
 	imgUrl: 'https://media.wincacademy.nl/kombucha.jpg',
 	alt: `Picture of a kombucha drink`,
 };
 
 const orangeJuice = {
-	id: 4,
+	id: self.crypto.randomUUID(),
 	name: 'Orange juice',
 	imgUrl: 'https://media.wincacademy.nl/orange.png',
 	alt: `Picture of a glass of orange juice`,
 };
 
 const water = {
-	id: 5,
+	id: self.crypto.randomUUID(),
 	name: 'Water',
 	imgUrl: 'https://media.wincacademy.nl/water.png',
 	alt: `Picture of a glass of water`,
 };
 
 const cola = {
-	id: 6,
+	id: self.crypto.randomUUID(),
 	name: 'Cola',
 	imgUrl: 'https://media.wincacademy.nl/cola.png',
 	alt: `Picture of a glass of cola`,
