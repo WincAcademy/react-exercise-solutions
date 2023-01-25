1d04a21de12e1f18d0783c5b2e0a369e436d676d
Mark van der Laan <mrkvdl@gmail.com>
2023-01-25 09:58:33 +0100

    fix: bug in react starter

--- react-basics-project-starter/react-basics-project-starter/src/pages/RecipesPage.jsx
+++ react-basics-project-starter/react-basics-project-starter/src/pages/RecipesPage.jsx
@@ -1,7 +1,7 @@
 import { Center, Heading } from '@chakra-ui/react';
 import { data } from '../utils/data';
 
-export const Recipes = () => {
+export const RecipesPage = () => {
 	return (
 		<Center h="100vh" flexDir="column">
 			<Heading>Your Recipe App</Heading>
