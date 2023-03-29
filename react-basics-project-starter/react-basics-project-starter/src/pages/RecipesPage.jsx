05296462aff361aeebe100adc6d5baddde3db27c
Mark van der Laan <mrkvdl@gmail.com>
2023-03-29 13:46:05 +0100

    fix: added small change of file and component name for clarity

--- react-basics-project-starter/react-basics-project-starter/src/pages/RecipesPage.jsx
+++ react-basics-project-starter/react-basics-project-starter/src/pages/RecipesPage.jsx
@@ -1,10 +1,10 @@
 import { Center, Heading } from '@chakra-ui/react';
 import { data } from '../utils/data';
 
-export const RecipesPage = () => {
-	return (
-		<Center h="100vh" flexDir="column">
-			<Heading>Your Recipe App</Heading>
-		</Center>
-	);
+export const RecipeListPage = () => {
+  return (
+    <Center h="100vh" flexDir="column">
+      <Heading>Your Recipe App</Heading>
+    </Center>
+  );
 };
