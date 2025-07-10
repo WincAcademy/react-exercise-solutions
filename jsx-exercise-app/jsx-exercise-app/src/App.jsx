965508a3960a3a974482a2aabe8823633bfc1566
Arthur Baars <arthurbaars@outlook.com>
2025-07-10 16:11:26 +0200

    Converted tabs to spaces

--- jsx-exercise-app/jsx-exercise-app/src/App.jsx
+++ jsx-exercise-app/jsx-exercise-app/src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css'
 
 function App() {
-	const greeting = "Welcome to React!"
-	const description = <p>This app is going to change your life.</p>
-	
-	return (
-		<div>
-			<h1>Hello World!</h1>
-			<p>{greeting}</p>
-			{description}
-		</div>
-	)
+    const greeting = "Welcome to React!"
+    const description = <p>This app is going to change your life.</p>
+    
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <p>{greeting}</p>
+            {description}
+        </div>
+    )
 }
 
 export default App
