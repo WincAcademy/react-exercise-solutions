2de98dafdb79d8db3a03372b10d25bee545bb5a8
Arthur Baars <arthurbaars@outlook.com>
2025-07-17 13:43:37 +0200

    Converted props-exercise-solution/DrinkChoice.jsx to spaces instead of tabs

--- props-exercise-solution/props-exercise-solution/src/components/DrinkChoice.jsx
+++ props-exercise-solution/props-exercise-solution/src/components/DrinkChoice.jsx
@@ -1,9 +1,9 @@
 export const DrinkChoice = ({ drink }) => {
-	return (
-		<>
-			<h2>Your choice: {drink.name}</h2>
-			<img src={drink.imgUrl} width={100} height={100} alt={drink.imgAlt} />
-			<p>Your drink will be ready in a few minutes</p>
-		</>
-	);
+    return (
+        <>
+            <h2>Your choice: {drink.name}</h2>
+            <img src={drink.imgUrl} width={100} height={100} alt={drink.imgAlt} />
+            <p>Your drink will be ready in a few minutes</p>
+        </>
+    );
 };
