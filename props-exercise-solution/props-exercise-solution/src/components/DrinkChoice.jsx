7146b90eae7a00c7b2a1caa6dc51aca6a1591b5b
Arthur Baars <arthurbaars@outlook.com>
2025-07-17 13:23:11 +0200

    Improved props-exercise-solution

--- props-exercise-solution/props-exercise-solution/src/components/DrinkChoice.jsx
+++ props-exercise-solution/props-exercise-solution/src/components/DrinkChoice.jsx
@@ -2,7 +2,7 @@ export const DrinkChoice = ({ drink }) => {
 	return (
 		<>
 			<h2>Your choice: {drink.name}</h2>
-			<img src={drink.imgUrl} width={100} height={100} alt={drink.alt} />
+			<img src={drink.imgUrl} width={100} height={100} alt={drink.imgAlt} />
 			<p>Your drink will be ready in a few minutes</p>
 		</>
 	);
--- props-exercise-solution/props-exercise-solution/src/utils/data.js
+++ props-exercise-solution/props-exercise-solution/src/utils/data.js
@@ -1,11 +1,11 @@
 export const tea = {
-	name: "Tea",
-	imgUrl: "https://media.wincacademy.nl/tea.jpeg",
-	alt: "Picture of a cup of tea"
+    name: "Tea",
+    imgUrl: "https://media.wincacademy.nl/tea.jpeg",
+    imgAlt: "Picture of a cup of tea"
 };
 
 export const coffee = {
-	name: "Coffee",
-	imgUrl: "https://media.wincacademy.nl/coffee.jpeg",
-	alt: "Picture of a cup of coffee"
+    name: "Coffee",
+    imgUrl: "https://media.wincacademy.nl/coffee.jpeg",
+    imgAlt: "Picture of a cup of coffee"
 };
