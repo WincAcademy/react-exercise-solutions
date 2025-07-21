77318ba70f6b4de95d7566b674e521048148d31e
Arthur Baars <arthurbaars@outlook.com>
2025-07-21 13:51:21 +0200

    Slightly changed indentation for props exercises (pt2)

--- props-example-01/props-example-01/src/components/WeatherIcon.jsx
+++ props-example-01/props-example-01/src/components/WeatherIcon.jsx
@@ -3,7 +3,11 @@ import './WeatherIcon.css';
 export const WeatherIcon = (props) => {
 	return (
         <>
-            <img className="weather-icon" src={props.imageSrc} alt="Weather icon" />
+            <img
+                className="weather-icon"
+                src={props.imageSrc}
+                alt="Weather icon"
+            />
             <h2>{props.weatherType}</h2>
         </>
 	);
--- props-example-02/props-example-02/src/components/WeatherIcon.jsx
+++ props-example-02/props-example-02/src/components/WeatherIcon.jsx
@@ -3,7 +3,11 @@ import './WeatherIcon.css';
 export const WeatherIcon = ({ imageSrc, weatherType }) => {
 	return (
         <>
-            <img className="weather-icon" src={imageSrc} alt="Weather icon" />
+            <img
+                className="weather-icon"
+                src={imageSrc}
+                alt="Weather icon"
+            />
             <h2>{weatherType}</h2>
         </>
 	);
