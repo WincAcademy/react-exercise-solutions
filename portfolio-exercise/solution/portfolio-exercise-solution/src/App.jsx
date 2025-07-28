dd1fdc72d5276c48cd8253897bac0f0b7ae44d4c
Arthur Baars <arthurbaars@grachtwerk.nl>
2025-07-28 15:02:20 +0200

    Modified portfolio-exercise-solution

--- portfolio-exercise/solution/portfolio-exercise-solution/src/App.jsx
+++ portfolio-exercise/solution/portfolio-exercise-solution/src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react';
 import './App.css';
+import { useState } from 'react';
 import { PortfolioItemPage } from './pages/PortfolioItemPage';
 import { PortfolioPage } from './pages/PortfolioPage';
 import { portfolioItems } from './utils/data';
 
 export const App = () => {
-  console.log(portfolioItems);
-  const [selectedItem, setSelectedItem] = useState(portfolioItems[0]);
+    console.log(portfolioItems);
+    const [selectedItem, setSelectedItem] = useState(portfolioItems[0]);
 
-  return (
-    <div className="App">
-      {selectedItem ? (
-        <PortfolioItemPage item={selectedItem} />
-      ) : (
-        <PortfolioPage />
-      )}
-    </div>
-  );
+    return (
+        <>
+            {selectedItem
+                ? <PortfolioItemPage item={selectedItem} />
+                : <PortfolioPage />
+            }
+        </>
+    );
 };
