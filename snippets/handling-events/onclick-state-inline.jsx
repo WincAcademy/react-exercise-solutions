940202c7ae9b6a35c542ec8e6f281e1c75fdde77
Arthur Baars <arthurbaars@outlook.com>
2025-07-31 14:01:05 +0200

    Added more onclick example code snippets

---
+++ snippets/handling-events/onclick-state-inline.jsx
@@ -0,0 +1,13 @@
+export const App = () => {
+    const [clicked, setClicked] = useState(false);
+
+    return (
+        <>
+            <button onClick={() => setClicked(true)}>
+                Click me!
+            </button>
+
+            {clicked && <p>Button was clicked!</p>}
+        </>
+    );
+};
---
+++ snippets/handling-events/onclick-state.jsx
@@ -0,0 +1,17 @@
+export const App = () => {
+    const [clicked, setClicked] = useState(false);
+
+    const handleClick = () => {
+        setClicked(true);
+    };
+
+    return (
+        <>
+            <button onClick={handleClick}>
+                Click me!
+            </button>
+
+            {clicked && <p>Button was clicked!</p>}
+        </>
+    );
+};
